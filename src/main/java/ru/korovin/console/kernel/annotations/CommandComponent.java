4b49c5c0d5f54b0f38fac4ddf94efc3f3f77b115
Egor <egorka_lolka23@mail.ru>
2025-05-05 15:38:52 +0400

    fix: add target param to annotations

--- src/main/java/ru/korovin/console/kernel/annotations/CommandComponent.java
+++ src/main/java/ru/korovin/console/kernel/annotations/CommandComponent.java
@@ -4,10 +4,13 @@ import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 @Component
 public @interface CommandComponent {
     @AliasFor(
